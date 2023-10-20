ecb4d9a78e1c7be2964f3099a87b7f75e5480947
teng <teng2002823@outlook.com>
2023-10-20 19:04:40 +0800

    init

---
+++ code-2/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is my name\n";
+    std::cout << "This is my address\n";
+    return 0;
+}
---
+++ code-2/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    long number;
+    std::cout << "Please input a number\n";
+    std::cin >> number;
+    std::cout << "convert to number = " << number * 220 << std::endl;
+    return 0;
+}
